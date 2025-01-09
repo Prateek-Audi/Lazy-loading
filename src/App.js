0a37ca1314b987904225b96733cb06454fc55107
Prateek Audichya <prateek.phppoets@gmail.com>
2025-01-10 00:53:21 +0530

    initial setup done

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import LandingPage from "./components/LandingPage";
+import MasterPage from "./components/MasterPage";
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/master" element={<MasterPage />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
